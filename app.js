9e464ee6edef8670c0ca8278e5d277e0abe5731f
Abbas Gawali <abbasgawali111@gmail.com>
2025-03-21 09:35:04 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+import express from "express"
+import cors from "cors"
+export const app = express();
+
+app.use(cors());
+
+
+
+app.get((req, res) => {
+    res.send("Hello from Uber clone backend");
+})
+
+
